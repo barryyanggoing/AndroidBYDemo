12b0af7a163e6fdaf09d35af6c4e93a055b0e54a
BarryYang <614994859@qq.com>
2020-07-05 11:34:22 +0800

    测试布局

--- app/src/main/java/com/barryyang/barryyangdemo/MainActivity.java
+++ app/src/main/java/com/barryyang/barryyangdemo/MainActivity.java
@@ -1,14 +1,28 @@
 package com.barryyang.barryyangdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Placeholder;
 
 import android.os.Bundle;
+import android.view.View;
 
+/**
+ * 测试各种
+ *
+ * @author barryyang
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constraintlayout_placeholder);
+        final Placeholder placeholder = findViewById(R.id.placeholder);
+        findViewById(R.id.TextView1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeholder.setContentId(R.id.TextView1);
+            }
+        });
     }
 }
